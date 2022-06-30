b9d73f7b49558a4c2e1cc4320427a06355a52fcf
yangzhe <2215088339@qq.com>
2022-06-30 13:38:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest<br>
+ * Description: <br>
+ *
+ * @author YZ <br>
+ * @version <br>
+ * @create 2022-06-30 13:32 <br>
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
